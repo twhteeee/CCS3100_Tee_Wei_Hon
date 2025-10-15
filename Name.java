b8155063d2a5e7fb1619fd5d0a5d483110061488
twhteeee <weihontee@gmail.com>
2025-10-15 13:16:38 +0800

    first commit

---
+++ Name.java
@@ -0,0 +1,6 @@
+public class Name{
+    //My name is Tee Wei Hon
+    public static void main(String[] args) {
+        System.out.println("Tee Wei Hon");
+    }
+}
---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student{
+    //My student information
+    public static void main(String[] args) {
+        String name = "Tee Wei Hon";
+        int studentID = 230773;
+        String address = "No 953, Jalan SJ9/10C, Taman Seremban Jaya, 70450 Seremban, Negeri Sembilan.";
+        System.out.println("Name: " + name);
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Address: " + address);
+    }
+}
