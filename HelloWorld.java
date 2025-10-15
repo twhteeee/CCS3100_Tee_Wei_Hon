a2884fe2ced2c1e15d2091e981790f7005360809
twhteeee <weihontee@gmail.com>
2025-10-15 13:32:01 +0800

    third

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
         public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
-        System.out.println("Hiiii");
+        System.out.println("Hiiiiii");
+        System.out.println("1+1");
     }
 }
